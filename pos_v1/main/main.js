9dc53d232af06215caa448556df3df5c99656f14
zhaolu <1632089314@qq.com>
2016-07-18 21:03:17 +0800

    simplified main.js

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -8,15 +8,11 @@ let buildCartItems = (tags, allItems) => {
     let barcode = splittedTag[0];
     let count = parseFloat(splittedTag[1] || 1);
 
-    let cartItem = cartItems.find((cartItem) => {
-      return cartItem.item.barcode === barcode;
-    });
+    let cartItem = cartItems.find(cartItem => cartItem.item.barcode === barcode);
     if (cartItem) {
       cartItem.count++;
     } else {
-      let item = allItems.find((item) => {
-        return item.barcode === barcode;
-      });
+      let item = allItems.find(item => item.barcode === barcode);
 
       cartItems.push({item: item, count: count});
     }
