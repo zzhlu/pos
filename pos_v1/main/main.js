1fc80b62c0d4bb8dc612cf347b8a9b804215fd7e
zhaolu <1632089314@qq.com>
2016-07-18 20:02:46 +0800

    build cartItems

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+let buildCartItems = (tags, allItems) => {
+  let cartItems = [];
+
+  for (let tag of tags) {
+    let splittedTag = tag.split('-');
+    let barcode = splittedTag[0];
+    let count = parseFloat(splittedTag[1] || 1);
+
+    let cartItem = cartItems.find((cartItem) => {
+      return cartItem.item.barcode === barcode;
+    });
+    if (cartItem) {
+      cartItem.count++;
+    } else {
+      let item = allItems.find((item) => {
+        return item.barcode === barcode;
+      });
+
+      cartItems.push({item: item, count: count});
+    }
+  }
+
+  return cartItems;
+};
