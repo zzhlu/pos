940374b98b478af30a314c58ee512425a78d103c
zhaolu <1632089314@qq.com>
2016-07-19 18:35:48 +0800

    simplified receiptItems

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,16 +1,5 @@
 'use strict';
 
-let printReceipt = (tags) => {
-  let allItems = loadAllItems();
-  let cartItems = buildCartItems(tags, allItems);
-  let promotions = loadPromotions();
-  let receiptItems = buildReceiptItems(cartItems, promotions);
-  let receipt = buildReceipt(receiptItems);
-  let receiptText = generateReceiptText(receipt);
-
-  console.log(receiptText);
-};
-
 let buildCartItems = (tags, allItems) => {
   let cartItems = [];
 
@@ -33,51 +22,26 @@ let buildCartItems = (tags, allItems) => {
 };
 
 let buildReceiptItems = (cartItems, promotions) => {
-  let receiptItems = [];
-
-  for (let cartItem of cartItems) {
-    let subtotal = cartItem.item.price * cartItem.count;
-    let save = 0;
-
-    let barcode = promotions[0].barcodes.find(barcode => barcode === cartItem.item.barcode);
-    if (barcode && cartItem.count >= 3) {
-      save = cartItem.item.price * parseInt(cartItem.count / 3);
-      subtotal -= save;
-    }
-
-    receiptItems.push({cartItem: cartItem, subtotal: subtotal, save: save});
-  }
-
-  return receiptItems;
+  return cartItems.map(cartItem => {
+    let promotionType = getPromotionType(cartItem.item.barcode, promotions);
+    let {saved, subtotal} = discount(cartItem, promotionType);
+    return {cartItem, saved, subtotal};
+  });
 };
 
-let buildReceipt = (receiptItems) => {
-  let actualTotal = 0;
-  let saveTotal = 0;
-
-  for (let receiptItem of receiptItems) {
-    actualTotal += receiptItem.subtotal;
-    saveTotal += receiptItem.save;
-  }
-
-  return {receiptItems: receiptItems, actualTotal: actualTotal, saveTotal: saveTotal};
+let getPromotionType = (barcode, promotions) => {
+  let promotion = promotions.find(promotion => promotion.barcodes.includes(barcode));
+  return promotion ? promotion.type : '';
 };
 
-let generateReceiptText = (receipt) => {
-  return `***<没钱赚商店>收据***
-${generateReceiptItemsText(receipt.receiptItems)}----------------------
-总计：${receipt.actualTotal.toFixed(2)}(元)
-节省：${receipt.saveTotal.toFixed(2)}(元)
-**********************`;
-};
+let discount = (cartItem, promotionType) => {
+  let subtotal = cartItem.item.price * cartItem.count;
+  let saved = 0;
 
-let generateReceiptItemsText = (receiptItems) => {
-  let receiptItemsText = ``;
-
-  for (let receiptItem of receiptItems) {
-    receiptItemsText += `名称：${receiptItem.cartItem.item.name}，数量：${receiptItem.cartItem.count}${receiptItem.cartItem.item.unit}，单价：${receiptItem.cartItem.item.price.toFixed(2)}(元)，小计：${receiptItem.subtotal.toFixed(2)}(元)
-`;
+  if (promotionType === 'BUY_TWO_GET_ONE_FREE') {
+    saved = cartItem.item.price * parseInt(cartItem.count / 3);
+    subtotal -= saved;
   }
 
-  return receiptItemsText;
+  return {subtotal, saved};
 };
