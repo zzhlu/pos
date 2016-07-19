07ace50794345322d8a1c6f67817f63bd2052b15
zhaolu <1632089314@qq.com>
2016-07-19 17:13:19 +0800

    finished first

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
+let printReceipt = (tags) => {
+  let allItems = loadAllItems();
+  let cartItems = buildCartItems(tags, allItems);
+  let promotions = loadPromotions();
+  let receiptItems = buildReceiptItems(cartItems, promotions);
+  let receipt = buildReceipt(receiptItems);
+  let receiptText = generateReceiptText(receipt);
+
+  console.log(receiptText);
+};
+
 let buildCartItems = (tags, allItems) => {
   let cartItems = [];
 
@@ -38,4 +49,35 @@ let buildReceiptItems = (cartItems, promotions) => {
   }
 
   return receiptItems;
+};
+
+let buildReceipt = (receiptItems) => {
+  let actualTotal = 0;
+  let saveTotal = 0;
+
+  for (let receiptItem of receiptItems) {
+    actualTotal += receiptItem.subtotal;
+    saveTotal += receiptItem.save;
+  }
+
+  return {receiptItems: receiptItems, actualTotal: actualTotal, saveTotal: saveTotal};
+};
+
+let generateReceiptText = (receipt) => {
+  return `***<没钱赚商店>收据***
+${generateReceiptItemsText(receipt.receiptItems)}----------------------
+总计：${receipt.actualTotal.toFixed(2)}(元)
+节省：${receipt.saveTotal.toFixed(2)}(元)
+**********************`;
+};
+
+let generateReceiptItemsText = (receiptItems) => {
+  let receiptItemsText = ``;
+
+  for (let receiptItem of receiptItems) {
+    receiptItemsText += `名称：${receiptItem.cartItem.item.name}，数量：${receiptItem.cartItem.count}${receiptItem.cartItem.item.unit}，单价：${receiptItem.cartItem.item.price.toFixed(2)}(元)，小计：${receiptItem.subtotal.toFixed(2)}(元)
+`;
+  }
+
+  return receiptItemsText;
 };
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -188,7 +188,7 @@ describe('pos', () => {
 
     spyOn(console, 'log');
 
-    printReceipt(inputs);
+    printReceipt(tags);
 
     const expectText = `***<没钱赚商店>收据***
 名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
