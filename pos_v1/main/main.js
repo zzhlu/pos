b40a9dc9207229b57f2ce416fdc99dd18b3579c7
zhaolu <1632089314@qq.com>
2016-07-19 19:37:06 +0800

    finished second

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,5 +1,16 @@
 'use strict';
 
+let printReceipt = (tags) => {
+  let allItems = loadAllItems();
+  let cartItems = buildCartItems(tags, allItems);
+  let promotions = loadPromotions();
+  let receiptItems = buildReceiptItems(cartItems, promotions);
+  let receipt = buildReceipt(receiptItems);
+  let receiptText = generateReceiptText(receipt);
+
+  console.log(receiptText);
+};
+
 let buildCartItems = (tags, allItems) => {
   let cartItems = [];
 
@@ -14,7 +25,7 @@ let buildCartItems = (tags, allItems) => {
     } else {
       let item = allItems.find(item => item.barcode === barcode);
 
-      cartItems.push({item: item, count: count});
+      cartItems.push({item, count});
     }
   }
 
@@ -24,8 +35,8 @@ let buildCartItems = (tags, allItems) => {
 let buildReceiptItems = (cartItems, promotions) => {
   return cartItems.map(cartItem => {
     let promotionType = getPromotionType(cartItem.item.barcode, promotions);
-    let {saved, subtotal} = discount(cartItem, promotionType);
-    return {cartItem, saved, subtotal};
+    let {subtotal, saved} = discount(cartItem, promotionType);
+    return {cartItem, subtotal, saved};
   });
 };
 
@@ -44,4 +55,35 @@ let discount = (cartItem, promotionType) => {
   }
 
   return {subtotal, saved};
+};
+
+let buildReceipt = (receiptItems) => {
+  let actualTotal = 0;
+  let savedTotal = 0;
+
+  for (let receiptItem of receiptItems) {
+    actualTotal += receiptItem.subtotal;
+    savedTotal += receiptItem.saved;
+  }
+
+  return {receiptItems, actualTotal, savedTotal};
+};
+
+let generateReceiptText = (receipt) => {
+  return `***<没钱赚商店>收据***
+${generateReceiptItemsText(receipt.receiptItems)}----------------------
+总计：${receipt.actualTotal.toFixed(2)}(元)
+节省：${receipt.savedTotal.toFixed(2)}(元)
+**********************`;
+};
+
+let generateReceiptItemsText = (receiptItems) => {
+  let receiptItemsText = ``;
+
+  for (let receiptItem of receiptItems) {
+    receiptItemsText += `名称：${receiptItem.cartItem.item.name}，数量：${receiptItem.cartItem.count}${receiptItem.cartItem.item.unit}，单价：${receiptItem.cartItem.item.price.toFixed(2)}(元)，小计：${receiptItem.subtotal.toFixed(2)}(元)
+`;
+  }
+
+  return receiptItemsText;
 };
